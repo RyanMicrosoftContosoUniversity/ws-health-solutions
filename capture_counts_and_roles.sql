125a291f06cdbbd5c0817f37a4e8cc02cc3fba0f
ryanMicrosoftTest <admin@mngenvmcap372892.onmicrosoft.com>
2025-06-06 09:13:17 -0400

    Update capture_counts_and_roles.sql

--- capture_counts_and_roles.sql
+++ capture_counts_and_roles.sql
@@ -1,7 +1,3 @@
--- Stored procedure to capture row counts for all tables and views
--- and list user-defined database roles. The results are written
--- to the table specified by @ResultTable.
-
 CREATE OR ALTER PROCEDURE dbo.CaptureCountsAndRoles
     @ResultTable SYSNAME
 AS
@@ -9,61 +5,55 @@ BEGIN
     SET NOCOUNT ON;
 
     -- Create result table if it does not exist
-    IF OBJECT_ID(@ResultTable) IS NULL
-    BEGIN
-        DECLARE @create NVARCHAR(MAX) = N'CREATE TABLE ' + QUOTENAME(@ResultTable) + N'(
-            ObjectType   NVARCHAR(20),
-            SchemaName   SYSNAME NULL,
-            ObjectName   SYSNAME NOT NULL,
-            RowCount     BIGINT NULL,
-            CaptureDate  DATETIME2 NOT NULL DEFAULT(SYSDATETIME())
-        );';
-        EXEC(@create);
-    END
+    DECLARE @create NVARCHAR(MAX) = N'
+        IF OBJECT_ID(''' + @ResultTable + N''') IS NULL
+        BEGIN
+            CREATE TABLE ' + QUOTENAME(@ResultTable) + N'(
+                ObjectType   NVARCHAR(20),
+                SchemaName   SYSNAME NULL,
+                ObjectName   SYSNAME NOT NULL,
+                RowCount     BIGINT NULL,
+                CaptureDate  DATETIME2 NOT NULL DEFAULT(SYSDATETIME())
+            );
+        END';
+    EXEC(@create);
 
-    DECLARE @schema SYSNAME;
-    DECLARE @name SYSNAME;
-    DECLARE @sql NVARCHAR(MAX);
+    -- Insert row counts for tables
+    DECLARE @insert_tables NVARCHAR(MAX) = N'
+        INSERT INTO ' + QUOTENAME(@ResultTable) + N' (ObjectType, SchemaName, ObjectName, RowCount)
+        SELECT
+            ''table'',
+            s.name,
+            t.name,
+            SUM(p.rows)
+        FROM sys.tables t
+        JOIN sys.schemas s ON t.schema_id = s.schema_id
+        JOIN sys.partitions p ON t.object_id = p.object_id
+        WHERE p.index_id IN (0,1)
+        GROUP BY s.name, t.name;';
+    EXEC(@insert_tables);
 
-    -- Collect table counts
-    DECLARE table_cur CURSOR FOR
-        SELECT s.name, t.name
-        FROM sys.tables AS t
-        JOIN sys.schemas AS s ON t.schema_id = s.schema_id;
-    OPEN table_cur;
-    FETCH NEXT FROM table_cur INTO @schema, @name;
-    WHILE @@FETCH_STATUS = 0
-    BEGIN
-        SET @sql = N'INSERT INTO ' + QUOTENAME(@ResultTable) + N'(ObjectType, SchemaName, ObjectName, RowCount)
-            SELECT ''table'', @schema, @name, COUNT(*) FROM ' + QUOTENAME(@schema) + N'.' + QUOTENAME(@name) + N';';
-        EXEC sp_executesql @sql, N'@schema SYSNAME, @name SYSNAME', @schema=@schema, @name=@name;
-        FETCH NEXT FROM table_cur INTO @schema, @name;
-    END
-    CLOSE table_cur;
-    DEALLOCATE table_cur;
-
-    -- Collect view counts
-    DECLARE view_cur CURSOR FOR
-        SELECT s.name, v.name
-        FROM sys.views AS v
-        JOIN sys.schemas AS s ON v.schema_id = s.schema_id;
-    OPEN view_cur;
-    FETCH NEXT FROM view_cur INTO @schema, @name;
-    WHILE @@FETCH_STATUS = 0
-    BEGIN
-        SET @sql = N'INSERT INTO ' + QUOTENAME(@ResultTable) + N'(ObjectType, SchemaName, ObjectName, RowCount)
-            SELECT ''view'', @schema, @name, COUNT(*) FROM ' + QUOTENAME(@schema) + N'.' + QUOTENAME(@name) + N';';
-        EXEC sp_executesql @sql, N'@schema SYSNAME, @name SYSNAME', @schema=@schema, @name=@name;
-        FETCH NEXT FROM view_cur INTO @schema, @name;
-    END
-    CLOSE view_cur;
-    DEALLOCATE view_cur;
-
-    -- Insert database roles
-    SET @sql = N'INSERT INTO ' + QUOTENAME(@ResultTable) + N'(ObjectType, SchemaName, ObjectName, RowCount)
+    -- Insert user-defined roles
+    DECLARE @insert_roles NVARCHAR(MAX) = N'
+        INSERT INTO ' + QUOTENAME(@ResultTable) + N'(ObjectType, SchemaName, ObjectName, RowCount)
         SELECT ''role'', NULL, name, NULL
         FROM sys.database_principals
         WHERE type = ''R'' AND name NOT LIKE ''db_%'';';
-    EXEC(@sql);
+    EXEC(@insert_roles);
+
+    -- Insert row counts for views (WITHOUT CURSORS)
+    DECLARE @sql NVARCHAR(MAX) = N'';
+    SELECT @sql = STRING_AGG(
+        'INSERT INTO ' + QUOTENAME(@ResultTable) + ' (ObjectType, SchemaName, ObjectName, RowCount)
+         SELECT ''view'', ' +
+         '''' + s.name + ''',' +
+         '''' + v.name + ''',' +
+         'COUNT(*) FROM ' + QUOTENAME(s.name) + '.' + QUOTENAME(v.name) + ';'
+    , CHAR(13) + CHAR(10))
+    FROM sys.views v
+    JOIN sys.schemas s ON v.schema_id = s.schema_id;
+
+    IF @sql IS NOT NULL AND LEN(@sql) > 0
+        EXEC(@sql);
 END
 GO
